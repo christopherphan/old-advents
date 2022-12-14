f0b74a7e2354a33581e423d916bb7053f365a710
Christopher Phan <cphan@chrisphan.com>
2022-12-13 23:10:19 -0600

    Add verbose mode to solution to 2015-17

--- 2015/17/solution.py
+++ 2015/17/solution.py
@@ -1,6 +1,5 @@
 """Solution to Day 17 of 2015 Advent of Code."""
 
-from collections import Counter
 from collections.abc import Sequence, Set
 from sys import argv
 from typing import Final
@@ -14,29 +13,30 @@ def _amount_held(all_containers: Sequence[int], selected: Set[int]) -> int:
 
 
 def possible_combos(
-    all_containers: Sequence[int], selected: Set[int], total_nog: int
-) -> Counter:
+    all_containers: Sequence[int], _selected: Set[int], total_nog: int
+) -> dict[int, frozenset[frozenset[int]]]:
     """
     Return the number of possible container combinations that include those in selected.
 
     The parameter ``selected`` is given as a set of INDICIES of all_containers.
     """
+    selected = frozenset(_selected)
     if (ah := _amount_held(all_containers, selected)) > total_nog:
-        return Counter()
+        return dict()
     elif ah == total_nog:
-        return Counter({len(selected)})
+        return {len(selected): frozenset([selected])}
     else:
         # We have to be careful not to overcount. We can avoid this by only adding
         # containers with indicies bigger than those in selected.
-        return sum(
-            [
-                possible_combos(all_containers, selected | {k}, total_nog)
-                for k in range(
-                    (max(selected) if selected else -1) + 1, len(all_containers)
-                )
-            ],
-            start=Counter(),
-        )
+        ret_dict: dict[int, frozenset[frozenset[int]]] = {
+            u + 1: frozenset() for u, _ in enumerate(all_containers)
+        }
+        for k in range((max(selected) if selected else -1) + 1, len(all_containers)):
+            for key, value in possible_combos(
+                all_containers, frozenset(list(selected) + [k]), total_nog
+            ).items():
+                ret_dict[key] |= value
+        return ret_dict
 
 
 def parse_input(text: str) -> list[int]:
@@ -44,12 +44,20 @@ def parse_input(text: str) -> list[int]:
     return [int(k.strip()) for k in text.splitlines()]
 
 
+def _fs_color(x: frozenset) -> str:
+    return (
+        "[magenta]{[/]"
+        + "[magenta],[/]".join(f"[cyan]{k}[/]" for k in x)
+        + "[magenta]}[/]"
+    )
+
+
 if __name__ == "__main__":
     c = Console()
     if len(argv) < 3:
         c.print(
             f"[white on red]Usage:[/] [yellow on black]{argv[0]}[/]"
-            + " [i]filename[/] [i]amt_to_store[/]"
+            + " [i]filename[/] [i]amt_to_store[/] [--verbose]"
         )
     else:
         with open(argv[1], "rt") as infile:
@@ -57,10 +65,30 @@ if __name__ == "__main__":
         total_nog = int(argv[2])
         containers = parse_input(data)
         pc = possible_combos(containers, set(), total_nog)
-        c.print(
-            "[white on dark_green]Part 1:[/]" + f" [yellow on black]{pc.total()}[/]"
-        )
+        total = sum(len(value) for value in pc.values())
+        c.print("[white on dark_green]Part 1:[/]" + f" [yellow on black]{total}[/]")
+        keys_with_solns = [k for k in pc.keys() if pc[k]]
+        best_num_conts = min(keys_with_solns) if total else None
+        if best_num_conts is not None:
+            min_container_combos = len(pc[best_num_conts])
+        else:
+            min_container_combos = 0
         c.print(
             "[white on dark_blue]Part 2:[/] "
-            + f"[yellow on black]{pc[min(pc.keys())]}[/]"
+            + f"[yellow on black]{min_container_combos}[/]"
         )
+        if len(argv) == 4 and argv[3] == "--verbose":
+            c.print(
+                "[white on blue]Containers:[/] "
+                + ", ".join(
+                    f"[cyan on black]#{idx}[/] ([green on black]{item}[/])"
+                    for idx, item in enumerate(containers)
+                )
+            )
+            for key, value in pc.items():
+                if value:
+                    c.print(
+                        f"[white on dark_green]{key} containers[/]"
+                        + f" ([yellow on black]{len(value)}[/]): "
+                        + ", ".join(_fs_color(k) for k in value)
+                    )
